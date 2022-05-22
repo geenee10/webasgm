903b8dcb99c14fc75c56265dd49923c9adb223f9
geenee10 <geeenee10@gmail.com>
2022-05-22 17:00:56 +0900

    error update

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,5 @@
+var dsh = window.parent.document.querySelector('.menut');
+
 //좌측 메뉴바 선택
 function mclk (obj) {
     var a = document.getElementsByClassName("men");
@@ -9,13 +11,27 @@ function mclk (obj) {
 const mtp = window.parent.document.querySelector('#maintitle p');
 const frm = document.querySelector('#rm');
 
+//메인으로 이동
 function goMain(){
     mtp.innerText = "Main";
+    for(var i = 0; i < dhs.length; i++){
+        dsh[i].classList.remove('sel');
+    }
+    window.parent.document.querySelector('.dsh').classList.add('sel');
 }
+//상품으로 이동
 function goProduct(){
     mtp.innerText = "Product";
+    for(var i = 0; i < dhs.length; i++){
+        dsh[i].classList.remove('sel');
+    }
+    window.parent.document.querySelector('.n').classList.add('sel');
 }
 //canvas로 이동
 function goCanvas (){
     mtp.innerText = "Canvas";
+    for(var i = 0; i < dhs.length; i++){
+        dsh[i].classList.remove('sel');
+    }
+    window.parent.document.querySelector('.pa').classList.add('sel');
 }
--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+var men = window.frames['lm'].document.getElementsByClassName("men");
+var s = window.frames['lm'].document.getElementsByClassName("s");
+var f = window.frames['lm'].document.getElementsByClassName("f");
+var t = window.frames['lm'].document.getElementsByClassName("t");
+
 //상단 메뉴바 선택
 function clk (obj) {
     var a = document.getElementsByClassName("menut");
@@ -8,21 +13,40 @@ function clk (obj) {
 }
 //지도로 이동
 function goMap (){
+    for(var i = 0; i < men.length; i++){
+        men[i].classList.remove('sel');
+    }
+    window.frames['lm'].document.getElementsByClassName('t').classList.add('sel');
     document.querySelector('#maintitle p').innerText = "Map";
 }
 //대시보드(홈)로 이동
 function goMain (){
+    for(var i = 0; i < men.length; i++){
+        men[i].classList.remove('sel');
+    }
+    window.frames['lm'].document.getElementsByClassName('f').classList.add('sel');
     document.querySelector('#maintitle p').innerText = "Main";
 }
 //공지사항으로 이동
 function goNotice (){
+    for(var i = 0; i < men.length; i++){
+        men[i].classList.remove('sel');
+    }
     document.querySelector('#maintitle p').innerText = "Notice";
 }
 //canvas로 이동
 function goCanvas (){
+    for(var i = 0; i < men.length; i++){
+        men[i].classList.remove('sel');
+    }
+    window.frames['lm'].document.getElementsByClassName('t').classList.add('sel');
     document.querySelector('#maintitle p').innerText = "Canvas";
 }
 //Product로 이동
 function goCanvas (){
+    for(var i = 0; i < men.length; i++){
+        men[i].classList.remove('sel');
+    }
+    window.frames['lm'].document.getElementsByClassName('s').classList.add('sel');
     document.querySelector('#maintitle p').innerText = "Product";
 }
