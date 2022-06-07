d1232e0ccb67ff881da923bf979357ed7effda9f
geenee10 <geeenee10@gmail.com>
2022-06-07 03:24:12 +0000

    product add, rm

--- script/product.js
+++ script/product.js
@@ -15,14 +15,20 @@ function al(){
     var chk = document.getElementsByClassName("ch");
     var st = document.getElementsByClassName("stat");
     var inf = document.querySelector("select");
+    var nsell = document.querySelector(".nsell");
+    var sell = document.querySelector(".sell");
     for(var i = 0; i < st.length; i++){
         if(chk[i].checked){
             if(st[i] == "판매중" && inf.value == "판매시작"){
                 alert("이미 판매중인 상품이 포함되어 있습니다.");
             } else if(inf.value == "판매시작"){
                 st[i].innerText = "판매중";
+                sell.innerText = parseInt(sell.innerText) + 1;
+                nsell.innerText = parseInt(nsell.innerText) - 1;
             } else if(inf.value == "판매중지"){
                 st[i].innerText = "판매중지";
+                sell.innerText = parseInt(sell.innerText) - 1;
+                nsell.innerText = parseInt(nsell.innerText) + 1;
             }
             
         }
